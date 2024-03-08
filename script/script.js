1c7f9b758aa639d7a705ab25007d80327b5c2606
nicolas <bigmacnls@gmail.com>
2024-03-07 23:50:07 -0300

    labs inc

--- script/script.js
+++ script/script.js
@@ -16,3 +16,21 @@ window.addEventListener("load", function () {
     video.play();
     video.loop = true;
 });
+
+// carousel
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    }
+})
